7f96820d81292d5b0a1a4b15aa8a99decfc108e1
Haitham Shahin <hshahin@redhat.com>
2021-02-13 12:33:00 -0500

    updated to webhooks and added automation

--- webhooks/build-webhook/app.py
+++ webhooks/build-webhook/app.py
@@ -23,12 +23,11 @@ def insert_build():
 
 def insert_build(build):
     print("submitting build: %s" % (build))
-    builds = mongo_client.build.builds
-    build_id = builds.insert_one(build).inserted_id
+    build_id = mongo_client.db.builds.insert_one(build).inserted_id
     print("submitted build id: %s" % (build_id))
 
-def create_mongo_client_connection(username, password, database):
-    uri = "mongodb://%s:%s@mongodb:27017/%s" % (username,password,database)
+def create_mongo_client_connection(username, password, host, database):
+    uri = "mongodb://%s:%s@%s:27017/%s" % (username,password,host, database)
     return MongoClient(uri)
 
 if __name__=='__main__':
@@ -36,6 +35,8 @@ if __name__=='__main__':
 
     mongo_username = os.environ.get('MONGODB_USER')
     mongo_password = os.environ.get('MONGODB_PASSWORD')
-    mongo_client = create_mongo_client_connection(mongo_username, mongo_password, 'build')
+    mongo_servicename = os.environ.get('MONGODB_SERVICE_HOST')
+    mongo_database = os.environ.get('MONGODB_DATABASE')
+    mongo_client = create_mongo_client_connection(mongo_username, mongo_password, mongo_servicename, mongo_database)
 
     app.run(host="0.0.0.0", port=8080)
