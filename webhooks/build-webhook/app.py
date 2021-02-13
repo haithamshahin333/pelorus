e60e68f1b4df01893f327441fc6b881ab99e3ae0
Haitham Shahin <hshahin@redhat.com>
2021-02-13 13:37:38 -0500

    updated

--- webhooks/build-webhook/app.py
+++ webhooks/build-webhook/app.py
@@ -4,7 +4,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-mongo_client = None
+db = None
 
 @app.route('/post/build', methods=['POST'])
 def insert_build():
@@ -23,12 +23,13 @@ def insert_build():
 
 def insert_build(build):
     print("submitting build: %s" % (build))
-    build_id = mongo_client.db.builds.insert_one(build).inserted_id
+    build_id = db.builds.insert_one(build).inserted_id
     print("submitted build id: %s" % (build_id))
 
 def create_mongo_client_connection(username, password, host, database):
-    uri = "mongodb://%s:%s@%s:27017/%s" % (username,password,host, database)
-    return MongoClient(uri)
+    uri = "mongodb://%s:%s@%s:27017/%s" % (username,password,host,database)
+    print(uri)
+    return MongoClient(uri)[database]
 
 if __name__=='__main__':
     print("starting app")
@@ -37,6 +38,6 @@ if __name__=='__main__':
     mongo_password = os.environ.get('MONGODB_PASSWORD')
     mongo_servicename = os.environ.get('MONGODB_SERVICE_HOST')
     mongo_database = os.environ.get('MONGODB_DATABASE')
-    mongo_client = create_mongo_client_connection(mongo_username, mongo_password, mongo_servicename, mongo_database)
+    db = create_mongo_client_connection(mongo_username, mongo_password, mongo_servicename, mongo_database)
 
     app.run(host="0.0.0.0", port=8080)
